65e8bd97e8b919397a105706fe0e90d9347ac28c
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-02-04 19:20:59 +0200

    change document title

--- src/TitleChangler.js
+++ src/TitleChangler.js
@@ -2,20 +2,18 @@ import { useState, useEffect } from "react";
 
 export default function TitleChangler(){
     const [ counter, setCounter ] = useState(1);
-    const [ counterEffect, setCounterEff ] = useState(1);
 
     const handleClick = (() => {
         setCounter((counter) => counter + 1);
     });
 
     useEffect(() => {
-        setCounterEff((counterEffect) => counterEffect = counter);
+        document.title = `Count: (${counter})`;       
     },[counter]);
     
     
     return (
         <div>
-            <h1>Count ({counterEffect})</h1>
             <button onClick={handleClick}>Count ({counter})</button>
         </div>
     );
